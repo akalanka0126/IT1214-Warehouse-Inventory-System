ec04b1923a5559a22854650c581ef2d48a08a129
Pasindu Akalanka <2022ict134@stu.vau.ac.lk>
2026-03-31 20:46:04 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,29 @@
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Product(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void display() {
+        System.out.println("ID: " + id +
+                ", Name: " + name +
+                ", Quantity: " + quantity);
+    }
+}
